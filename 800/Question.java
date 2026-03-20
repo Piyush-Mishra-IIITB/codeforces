07d9d0ec608b30ffddd585c95da136657d870a87
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-20 16:26:26 +0530

    Seating in a bus

--- 800/Question.java
+++ 800/Question.java
@@ -1,1 +1,29 @@
 // All question of 800 ratings are here
+
+B. Seating in a Bus
+
+class Main {
+    public static void seating(int n, int arr[]) {
+        boolean vis[] = new boolean[n + 2]; // safe bounds
+
+        vis[arr[0]] = true;
+
+        for (int k = 1; k < n; k++) {
+            int seat = arr[k];
+
+            if (vis[seat - 1] || vis[seat + 1]) {
+                vis[seat] = true;
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 1};
+        seating(3, arr);
+    }
+}
