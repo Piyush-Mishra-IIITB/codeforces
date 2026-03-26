ac703507ad4c341bc13cba863239bbcb90c5d463
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-26 09:58:53 +0530

    81. Search in Rotated Sorted Array II

--- 800/Question.java
+++ 800/Question.java
@@ -692,3 +692,40 @@ class Solution {
         return min;
     }
 }
+
+// 81. Search in Rotated Sorted Array II
+
+
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid]==target){
+                return true;
+            }
+              if(arr[mid]==arr[start]&& arr[mid]==arr[end]){
+                start++;
+                end--;
+              }
+              else if(arr[mid]>=arr[start]){
+                  if(arr[start]<=target && target<=arr[mid]){
+                    end=mid-1;
+                  }else{
+                    start=mid+1;
+                  }
+              }
+              else if(arr[mid]<=arr[end]){
+                if(arr[mid]<=target && target<=arr[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+              }
+            }
+        
+        return false;
+    }
+}
