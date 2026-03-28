5d17b559c692afd73c4abf20b5f1e561a0e89b1b
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-28 16:49:11 +0530

    optimized way 1482. Minimum Number of Days to Make m Bouquets

--- 800/Question.java
+++ 800/Question.java
@@ -895,3 +895,52 @@ class Solution {
         }
     }
 }
+
+// optimized way 1482. Minimum Number of Days to Make m Bouquets
+
+
+class Solution {
+    public int minDays(int[] nums, int m, int k) {
+        if((long)k * m > nums.length){
+            return -1;
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+        }
+        int start=1;
+        int end=max;
+        int output=max;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(helper(m,k,mid,nums)){
+                output=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        
+        return output;
+    }
+    public static boolean helper(int m,int k,int j,int arr[]){
+        
+        int z=k;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=j){
+                z--;
+            }else{
+                z=k;
+            }
+            if(z==0){
+                m--;
+                z=k;
+            }
+            if(m==0){
+                return true;
+                
+            }
+        }
+        return m==0;
+    }
+}
