6c2657d94ad9a1fe757419cb62b98e59bad3884a
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-28 17:06:44 +0530

    optimized way of 1283. Find the Smallest Divisor Given a Threshold

--- 800/Question.java
+++ 800/Question.java
@@ -944,3 +944,34 @@ class Solution {
         return m==0;
     }
 }
+
+
+// optimized way of 1283. Find the Smallest Divisor Given a Threshold
+
+
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+        }
+        int start=1;
+        int end=max;
+        int output=max;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int sum=0;
+            for(int i=0;i<nums.length;i++){
+            sum += ((nums[i] + mid - 1) / mid);
+            }
+            if(sum>threshold){
+                start=mid+1;
+            }else{
+                output=Math.min(output,mid);
+                end=mid-1;
+            }
+
+        }
+        return output;
+    } 
+}
