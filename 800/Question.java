a4f6e20b2f9a7c3bae1d14e7b5740df2f332a12c
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-30 18:39:04 +0530

    linkedlist rev

--- 800/Question.java
+++ 800/Question.java
@@ -1061,3 +1061,38 @@ class Main {
         System.out.println(p(arr,4));
     }
 }
+
+
+//llinkedlist
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       if(head ==null||head.next==null){
+        return head;
+       }
+        ListNode temp=head;
+        ListNode prev=null;
+        ListNode fr=head.next;
+        while(temp!=null){
+                temp.next=prev;
+                prev=temp;
+                temp=fr;
+                if(fr.next==null){
+                    break;
+                }
+                fr=fr.next;
+        }
+        temp.next=prev;
+        return temp;
+    }
+}
