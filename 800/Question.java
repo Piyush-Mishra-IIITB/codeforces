8b12943eeb73853b68155ed61ed6edd425c00d3a
Piyush Mishra <piyushmishra8982@gmail.com>
2026-03-31 09:08:52 +0530

    328 optimize code

--- 800/Question.java
+++ 800/Question.java
@@ -1189,3 +1189,31 @@ class Solution {
     }
     
 }
+
+// optimize code 
+
+
+     class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;         
+        ListNode even = head.next;     
+        ListNode evenHead = even;     
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;     
+            odd = odd.next;
+
+            even.next = odd.next;     
+            even = even.next;
+        }
+
+        odd.next = evenHead;          
+        return head;
+    }
+}
+
+    
