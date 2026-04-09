9140be2d951c9f3791d5512607963b4c03380c0d
Piyush Mishra <piyushmishra8982@gmail.com>
2026-04-09 10:11:02 +0530

    optimal sol of next permutation

--- 800/Question.java
+++ 800/Question.java
@@ -1986,3 +1986,48 @@ class Solution {
         }
     }
 }
+
+// next permutation
+
+
+class Solution {
+    public void nextPermutation(int[] arr) {
+        int index=-1;
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            Arrays.sort(arr);
+            return;
+        }
+    
+        for(int i=arr.length-1;i>index;i--){
+            if(arr[i]>arr[index]){
+            swap(arr,i,index);
+            break;
+            }
+        }
+      reverse(arr,index+1);
+      
+
+    }
+    public static void swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void reverse(int arr[],int i){
+        int start=i;
+        int end=arr.length-1;
+        while(start<=end){
+            int t=arr[start];
+            arr[start]=arr[end];
+            arr[end]=t;
+            start++;
+            end--;
+        }
+    }
+}
