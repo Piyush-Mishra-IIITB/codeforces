b39790a6efddf5a54a82ef43bee63b661ae6c6fa
Piyush Mishra <piyushmishra8982@gmail.com>
2026-04-11 10:47:00 +0530

    diameter of binary tree

--- 800/Question.java
+++ 800/Question.java
@@ -2170,3 +2170,30 @@ public class Main {
         }
     }
 }
+
+
+//diameter of binary tree
+
+
+class Solution {
+     int sum=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int leftH=height(root.left);
+        int rightH=height(root.right);
+        sum=Math.max(leftH+rightH,sum);
+        diameterOfBinaryTree(root.left);
+        diameterOfBinaryTree(root.right);
+        return sum;
+    }
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        return Math.max(left,right)+1;
+    }
+}
